aa7df57ce80eba0be52c4862c5586306a7ca5f57
diqksrk123 <diqksrk123@naver.com>
2023-04-09 18:23:05 +0900

    init

---
+++ src/main/java/com/example/springbatchpractise/SpringBatchPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchpractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchPractiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbatchpractise/SpringBatchPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchpractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPractiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
